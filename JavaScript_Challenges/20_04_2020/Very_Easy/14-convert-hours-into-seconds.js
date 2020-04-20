2efb169be49b49679d44df7accf0e3d27eb277ad
Sipo Charles <sipocharles@yahoo.co.uk>
2020-04-20 03:07:30 +0100

    Convert Hours into Seconds

---
+++ JavaScript_Challenges/20_04_2020/Very_Easy/14-convert-hours-into-seconds.js
@@ -0,0 +1,36 @@
+/**
+ *  Convert Hours into Seconds
+ *  ==========================
+ */
+
+/*
+    #language_fundamentals #maths #numbers
+    --------------------------------------
+
+    Write a function that converts hours into seconds
+
+    Examples:
+
+    howManySeconds(2) ➞ 7200
+
+    howManySeconds(10) ➞ 36000
+
+    howManySeconds(24) ➞ 86400
+
+    Notes:
+
+    - 60 seconds in a minute, 60 minutes in an hour
+    - Don't forget to return your answer.
+*/
+
+//  Solution
+
+function howManySeconds(hours) {
+	return (hours * 60 * 60);
+}
+
+//  Another Solution
+
+function howManySeconds(hours) {
+	return hours * 3600;
+}

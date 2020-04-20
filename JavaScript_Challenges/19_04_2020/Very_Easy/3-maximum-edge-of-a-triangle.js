fc6644ca7939a6ac143f11c85f4c77749a6fcb82
Sipo Charles <sipocharles@yahoo.co.uk>
2020-04-20 01:16:11 +0100

    Maximum edge of a Triangle

---
+++ JavaScript_Challenges/19_04_2020/Very_Easy/3-maximum-edge-of-a-triangle.js
@@ -0,0 +1,31 @@
+/**
+ *  Maximum edge of a Triangle
+ *  ==========================
+ */
+
+/*
+    #algortithms #maths #numbers
+    ----------------------------
+
+    Create a function that finds the maximum range of a triangle's third edge, where the side lengths are all integers.
+
+    Examples:
+
+    nextEdge(8, 10) ➞ 17
+
+    nextEdge(5, 7) ➞ 11
+
+    nextEdge(9, 2) ➞ 10
+
+    Notes:
+
+    - (side1 + side2) - 1 = maximum range of third edge.
+    - The side lengths of the triangle are positive integers.
+    - Don't forget to return the result.
+*/
+
+//  Solution
+
+function nextEdge(side1, side2) {
+	return (side1 + side2) - 1;
+}

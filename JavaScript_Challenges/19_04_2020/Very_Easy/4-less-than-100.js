bb2b442436970dc6f7f4e2ad470ddcaa7c30138a
Sipo Charles <sipocharles@yahoo.co.uk>
2020-04-20 01:16:55 +0100

    Less Than 100?

---
+++ JavaScript_Challenges/19_04_2020/Very_Easy/4-less-than-100.js
@@ -0,0 +1,25 @@
+/**
+ *  Less Than 100?
+ *  ==============
+ */
+
+/*
+    #language_fundamentals #maths #validation
+    -----------------------------------------
+
+    Given two numbers, return true if the sum of both numbers is less than 100. Otherwise return false.
+
+    Examples:
+
+    lessThan100(22, 15) ➞ true
+    // 22 + 15 = 37
+
+    lessThan100(83, 34) ➞ false
+    // 83 + 34 = 117
+*/
+
+//  Solution
+
+function lessThan100(a, b) {
+    return a + b < 100;
+}

55721278a20e50e8731e008fb96d286bdaab6a4d
Sipo Charles <sipocharles@yahoo.co.uk>
2020-04-20 01:21:28 +0100

    Return the First Element in an Array

---
+++ JavaScript_Challenges/19_04_2020/Very_Easy/9-return-the-first-element-in-an-array.js
@@ -0,0 +1,29 @@
+/**
+ *  Return the First Element in an Array
+ *  ====================================
+ */
+
+/*
+    arrays - language_fundamentals
+    ------------------------------
+
+    Create a function that takes an array and returns the first element.
+
+    Examples:
+
+    getFirstValue([1, 2, 3]) ➞ 1
+
+    getFirstValue([80, 5, 100]) ➞ 80
+
+    getFirstValue([-500, 0, 50]) ➞ -500
+
+    Notes: 
+
+    The first element in an array has an index of 0.
+*/
+
+//  Solution
+
+function getFirstValue(arr) {
+	return(arr[0]);
+}

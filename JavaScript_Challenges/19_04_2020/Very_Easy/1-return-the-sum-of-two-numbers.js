e01cfa09bf653061856c10e555c05bee9381fd01
Sipo Charles <sipocharles@yahoo.co.uk>
2020-04-20 01:11:24 +0100

    Return the Sum of Two Numbers

---
+++ JavaScript_Challenges/19_04_2020/Very_Easy/1-return-the-sum-of-two-numbers.js
@@ -0,0 +1,26 @@
+/**
+ *  Return the Sum of Two Numbers
+ *  =============================
+ */
+
+/*
+    #algebra #language #fundamentals #maths #numbers
+    ------------------------------------------------
+
+    Create a function that takes two numbers as arguments and return their sum.
+
+    Examples:
+    
+    addition(3, 2) ➞ 5
+
+    addition(-3, -6) ➞ -9
+
+    addition(7, 3) ➞ 10
+*/
+
+
+// Solution
+
+function addition(a, b) {
+	return a + b;
+}
